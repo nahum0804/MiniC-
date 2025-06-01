7ed2ff3cdd7a5eaf355cc220bd35b48d80ef635b
CodeG-2021 <codeg2016cr@gmail.com>
2025-05-31 20:18:55 -0600

    version 2

--- MiniCSharp/MiniCSharp/Program.cs
+++ MiniCSharp/MiniCSharp/Program.cs
@@ -24,32 +24,26 @@ class Program
                                                  
                                      """;
 
-            // 1) LEXER con captura de errores léxicos (si ya tienes tu CustomErrorListener, regístralo aquí)
             var inputStream = new AntlrInputStream(inputText);
             var lexer = new MiniCSLexer(inputStream);
             lexer.RemoveErrorListeners();
             lexer.AddErrorListener(new LexerErrorListener());
 
-            // 2) TOKEN STREAM
             var tokens = new CommonTokenStream(lexer);
-
-            // 3) PARSER con captura de errores sintácticos
+            
             var parser = new MiniCSParser(tokens);
             parser.RemoveErrorListeners();
             parser.AddErrorListener(new ParserErrorListener());
 
-            // 4) INVOCAR la regla inicial “program”
             var tree = parser.program();
 
             Console.WriteLine("=== Árbol sintáctico ===");
             Console.WriteLine(tree.ToStringTree(parser));
             Console.WriteLine("========================\n");
 
-            // 5) Construir la tabla de símbolos con nuestro SymbolTableVisitor
             var symVisitor = new SymbolTableVisitor();
             symVisitor.Visit(tree);
 
-            // 6) Imprimir la tabla para depurar (verás variables, métodos, niveles, etc.)
             var table = symVisitor.Table;
             table.Print();
 
